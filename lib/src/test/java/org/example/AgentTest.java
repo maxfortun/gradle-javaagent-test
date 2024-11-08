520d048425302f7f37dacb678e703800fcfe94c9
Max <max@maxf.net>
2024-11-08 08:20:31 -0500

    updated

--- lib/src/test/java/org/example/AgentTest.java
+++ lib/src/test/java/org/example/AgentTest.java
@@ -7,13 +7,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AgentTest {
-    @Test public void agentInitializedFirst() {
+    @Test public void agentInitializedFirstTest() {
 		System.out.println("test enter: "+Agent.countDownLatch.getCount());
+
 		try {
 			Agent.countDownLatch.await();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+
 		System.out.println("test exit: "+Agent.countDownLatch.getCount());
     }
 }
